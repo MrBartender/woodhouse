419a743f2893a2dca81b2ab5a716340948109495
Jake Mitchell <mitchellja97@gmail.com>
2019-09-12 17:37:31 -0400

    add queue iterator

--- index.js
+++ index.js
@@ -11,27 +11,50 @@ const thingShadows = awsIot.thingShadow({
   host: 'a2deqfzrx988q4-ats.iot.us-west-2.amazonaws.com'
 })
 
+// Helper function to send updates and flag errors
+function sendUpdate(desired) {
+  const clientTokenUpdate = thingShadows.update(clientName, {
+    'state': {
+      'desired': desired
+    }
+  })
+
+  // If null, the shadow is currently updating and should try again
+  if (clientTokenUpdate === null) {
+    console.log('update shadow failed, operation still in progress');
+  }
+}
 // When we start, tell the backend we are live, subscribe to the shadow
 thingShadows.on('connect', () => {
   thingShadows.register(clientName, {}, () => {
-    const clientTokenUpdate = thingShadows.update(clientName, {
-      'state': {
-        'desired': {
-          'queue': []
-        }
-      }
-    })
-
-    // If null, the shadow is currently updating and should try again
-    if (clientTokenUpdate === null) {
-      console.log('update shadow failed, operation still in progress');
-    }
+    sendUpdate({ 'queue': [] })
   })
 })
 
 // Status events
-thingShadows.on('status', (thingName, stat, _clientToken, stateObject) => {
-  console.log('received ' + stat + ' on ' + thingName + ': ' + JSON.stringify(stateObject))
+thingShadows.on('status', (thingName, status, _clientToken, stateObject) => {
+  console.log('received ' + status + ' on ' + thingName + ': ' + JSON.stringify(stateObject))
+
+  // Use logic when a clean request and a state is desired
+  if status === 'accepted' && stateObject && stateObject.desired) {
+    const state = stateObject.desired.state;
+    console.log('inside accepted; state is: ' + state)
+
+    // Handle a queued order
+    if (state && state.queue && state.queue.length > 0) {
+      const { queue } = state
+
+      console.log('inside queue breaker, queue is: ' + queue)
+      const order = queue.shift()
+
+      // Handle pour logic
+      console.log('pouring: ' + JSON.stringify(order))
+      console.log('updating queue, new queue: ' + queue)
+
+      // Save the queue after removing one
+      sendUpdate({ 'queue': queue })
+    }
+  }
 })
 
 // Delta events
